b2e056c59c67d63b2d3e1dd3fbf82fcb7ad9fe54
pandane <40155609+pandane@users.noreply.github.com>
2018-06-12 20:21:19 -0600

    Added stdout feedback.

--- no_u.py
+++ no_u.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-#coding: utf8 
+# coding: utf8
 
-import tweepy, time, sys, re, json
+import tweepy
+import time
+import sys
+import re
+import json
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
@@ -9,6 +13,7 @@ from tweepy.streaming import StreamListener
 from credentials import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET, USER_HANDLE
 
 import random
+
 
 class StdOutListener(StreamListener):
     def __init__(self):
@@ -21,54 +26,64 @@ class StdOutListener(StreamListener):
 
     def on_status(self, status):
         try:
-            #content of responses is randomly chosen from this list:
-            tweetResponses = ['no u', 'NO UU', 'NO U', 'nO U', 'NO U!!', '\nNO U\nO\n\nU']
+            # content of responses is randomly chosen from this list:
+            tweetResponses = ['no u', 'NO UU', 'NO U',
+                              'nO U', 'NO U!!', '\nNO U\nO\n\nU']
 
-            #ignore whitespace and upper/lower cases
+            # ignore whitespace and upper/lower cases
             statusText = " ".join(status.text.split()).lower()
 
-            #don't respond to self
+            # don't respond to self
             if status.author.screen_name == USER_HANDLE.strip("@"):
                 return
 
-            #patterns that trigger a response:
+            # do not reply to retweets
+            if "RT:" in statusText:
+                return
+
+            # patterns that trigger a response:
             triggers = ['no u', 'no you']
 
-            #check if reply contains triggers
+            # check if reply contains triggers
             triggerNotActivated = True
             for trigger in triggers:
                 if trigger in statusText:
                     triggerNotActivated = False
 
-            #do not reply if there are no triggers present
+            print("@"+status.author.screen_name+" replied to you:")
+            print(status.text)
+
+            # do not reply if there are no triggers present
             if triggerNotActivated:
                 return
 
-            #do not reply to retweets
-            if "RT:" in statusText:
-                return
+            # contents of reply message:
+            postMsg = '@' + status.author.screen_name + \
+                " " + random.choice(tweetResponses)
 
-            #contents of reply message:
-            postMsg = '@' + status.author.screen_name + " " + random.choice(tweetResponses)
-
-            #limit to a chain of 5 responses
+            # limit to a chain of 5 responses
             if(status.author.screen_name == self.lastTweeted):
                 self.lastTweetedCount += 1
             else:
                 self.lastTweeted = status.author.screen_name
                 self.lastTweetedCount = 0
 
-            if(self.lastTweetedCount > 5):
+            if(self.lastTweetedCount > 6):
                 return
 
-            #rate limiting detection
+            # rate limiting detection
             try:
                 if(self.wait > 10):
                     return
                 elif(self.wait > 0):
                     time.sleep(self.wait)
 
-                #post reply
+                # post reply
+                print("")
+                print("You ("+USER_HANDLE+") replied with:")
+                print(postMsg)
+                print("")
+
                 self.api.update_status(postMsg, status.id)
                 self.wait = 0
             except Exception as rateLimitException:
@@ -82,6 +97,7 @@ class StdOutListener(StreamListener):
 
     def on_error(self, status):
         print(status)
+
 
 if __name__ == '__main__':
     listener = StdOutListener()
